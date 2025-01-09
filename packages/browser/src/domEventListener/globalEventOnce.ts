df4f103a7486a5b08961a9e78c50732541aa000e
tanghaojie <thj3360016@gmail.com>
2025-01-09 13:43:10 +0800

    feat: Update.

---
+++ packages/browser/src/domEventListener/globalEventOnce.ts
@@ -0,0 +1,30 @@
+import { on } from './on'
+import { off } from './off'
+
+export function useGlobalEventOnce(
+  event: string,
+  cb?: (e?: Event) => void,
+  globalEl: HTMLElement | Document | Window = document,
+  option?: {
+    stop?: boolean
+    prevent?: boolean
+  }
+) {
+  function registerOnce() {
+    on(globalEl, event, onGlobalOnce)
+  }
+
+  function onGlobalOnce(e: Event) {
+    const { stop = false, prevent = false } = option || {}
+    prevent && e.preventDefault()
+    stop && e.stopPropagation()
+
+    cb && cb(e)
+
+    off(globalEl, event, onGlobalOnce)
+  }
+
+  return {
+    registerOnce,
+  }
+}
